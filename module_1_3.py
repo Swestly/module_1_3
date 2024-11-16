3fdfebe10174ac4461b99de9de78178fcfb35985
Swestly <brat02rus@gmail.com>
2024-11-16 13:33:22 +0300

    Add files via upload

---
+++ module_1_3.py
@@ -0,0 +1,12 @@
+
+name = 'Ilya'
+age = 23
+age_new = 24
+is_student = True
+
+
+# Использование переменных
+print( f'Name: {name}' )
+print ( f'Age: {age}' )
+print( f'Age New: {age_new}' )
+print( f'Is Student {is_student}' )
